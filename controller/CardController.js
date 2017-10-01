c3555aec27a7eaadc2ca83f1857aa73a072f4f3c
chanpan <chanpan.nuttaphon@gmail.com>
2017-10-01 12:13:37 +0700

    smart card reader

---
+++ controller/CardController.js
@@ -0,0 +1,16 @@
+const util = require('../shared/utils/Utils.js');
+const file = require("../shared/files/Files.js");
+Init();
+function CopyDefaultDataBase(){
+    var target = util.HomePath()+"/cards/card.db";
+    var src = "./assets/card_default.db";
+     
+    file.CopyFile(target,src).subscribe((res)=>{
+        console.log(res);
+    });
+}
+
+function Init(){
+    CopyDefaultDataBase();
+}
+
---
+++ main.js
@@ -0,0 +1,44 @@
+const electron = require("electron");
+const {app, BrowserWindow,ipcMain} = electron;
+
+let mainWin="";
+let child="";
+
+app.on("ready", ()=>{
+	mainWin = new BrowserWindow({ 
+    width:1200, height:600,
+   
+  });
+	mainWin.loadURL(`file://${__dirname}/index.html`);
+	app.on('closed', function () { mainWin = null; app.exit() });
+  mainWin.toggleDevTools();
+});
+app.on('window-all-closed', () => {
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+
+});
+exports.childWindow = function(ezf_id){
+ child = new BrowserWindow({ 
+  width:1024, height:600,
+  minWidth:1024,
+  minHeight: 600,
+  parent: mainWin, 
+  modal: true,
+  show: true
+});
+ child.loadURL(`file://${__dirname}/index2.html?ezf_id=${ezf_id}`);
+ child.toggleDevTools();
+
+ app.on('closed', function () { child = null; app.exit() });
+}
+app.on('activate', () => {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
